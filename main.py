65679ca86c50410df055a878c5698c8350b3975b
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-05-13 12:23:59 +0530

    create main file

---
+++ main.py
@@ -0,0 +1,37 @@
+import DoublyLinkedList
+
+linkedList=DoublyLinkedList.DoublyLinkedList()
+linkedList.printForwardList()
+
+print("\nDoubly Linked List is :")
+
+linkedList.addNodeBeginning(3)
+linkedList.addNodeBeginning(2)
+linkedList.addNodeBeginning(1)
+linkedList.addNodeEnd(5)
+linkedList.addNodeBeginning(0)
+linkedList.addNodeEnd(6)
+linkedList.addNodeInPos(4,4)
+
+linkedList.printForwardList()
+
+print("\nDeleted First Node in Doubly Linked List :")
+linkedList.deleteFirstNode()
+linkedList.printForwardList()
+
+print("\nDeleted given position Node in Doubly Linked List :")
+linkedList.deleteNodeByPos(3)
+linkedList.printForwardList()
+
+print("\nDeleted Last Node in Doubly Linked List :")
+linkedList.deleteLastNode()
+linkedList.printForwardList()
+
+
+print("\nDoubly Linked List Print Backward")
+linkedList.printBackwardList()
+
+print("\n\nDoubly Linked List Element Search")
+linkedList.searchNodeByPos(5)
+
+
