111a7ceadd3df3caf0888cc27bcae0ac12dde02f
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-05-17 11:16:29 +0530

    change print line

--- main.py
+++ main.py
@@ -5,33 +5,33 @@ linkedList.printForwardList()
 
 print("\nDoubly Linked List is :")
 
-linkedList.addNodeBeginning(9)
-linkedList.addNodeBeginning(8)
-linkedList.addNodeBeginning(7)
-linkedList.addNodeBeginning(3)
-linkedList.addNodeBeginning(2)
-linkedList.addNodeBeginning(1)
-linkedList.addNodeBeginning(0)
-linkedList.addNodeEnd(13)
-linkedList.addNodeInPos(7,10)
+linkedList.addNodeBeginning(18)
+linkedList.addNodeBeginning(17)
+linkedList.addNodeBeginning(16)
+linkedList.addNodeBeginning(15)
+linkedList.addNodeBeginning(13)
+linkedList.addNodeBeginning(12)
+linkedList.addNodeBeginning(11)
+linkedList.addNodeInPos(3,14)
+linkedList.addNodeEnd(19)
 linkedList.printForwardList()
 
-print("\nDoubly Linked List Print Backward")
+print("\n\nDoubly Linked List Print Backward :")
 linkedList.printBackwardList()
 
-print("\nDeleted First Node in Doubly Linked List :")
+print("\n\nDeleted First Node :")
 linkedList.deleteFirstNode()
 linkedList.printForwardList()
 
-print("\nDeleted given position Node in Doubly Linked List :")
+print("\n\nDeleted given position Node :")
 linkedList.deleteNodeByPos(3)
 linkedList.printForwardList()
 
-print("\nDeleted Last Node in Doubly Linked List :")
+print("\n\nDeleted Last Node :")
 linkedList.deleteLastNode()
 linkedList.printForwardList()
 
 print("\n\nDoubly Linked List Element Search")
-linkedList.searchValue(2)
+linkedList.searchValue(17)
 
 
